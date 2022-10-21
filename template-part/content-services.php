87ad0cdda284c0a88f43c766e20c0fd24fce59c1
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-10-22 00:39:24 +0600

    Post Loop Creation

--- template-part/content-services.php
+++ template-part/content-services.php
@@ -10,28 +10,72 @@
                             </div>
 
                             <div class="row pt-lg-5">
-                                <div class="col-lg-6 col-12">
-                                    <div class="services-thumb">
-                                        <div class="d-flex flex-wrap align-items-center border-bottom mb-4 pb-3">
-                                            <h3 class="mb-0">Websites</h3>
 
-                                            <div class="services-price-wrap ms-auto">
-                                                <p class="services-price-text mb-0">$2,400</p>
-                                                <div class="services-price-overlay"></div>
+                                <?php
+
+                                // the query (for Post Loop)
+
+                                    $args = array(
+                                        'post_type' => 'customservice'
+                                    );
+
+                                    $the_query = new WP_Query( $args ); ?>
+
+                                    <?php if ( $the_query->have_posts() ) : ?>
+
+                                        <!-- pagination here -->
+
+                                        <!-- the loop -->
+                                        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+                                        <!-- HTML Loop Code -->
+                                        <div class="col-lg-6 col-12">
+                                            <div class="services-thumb">
+                                                <div class="d-flex flex-wrap align-items-center border-bottom mb-4 pb-3">
+                                                    <h3 class="mb-0"><?php the_title(); ?></h3>
+
+                                                    <div class="services-price-wrap ms-auto">
+                                                        <?php
+                                                            $postid = $the_query->post->ID;
+
+                                                            $price = get_post_meta($postid, 'Price', true);
+                                                        ?>
+                                                        <p class="services-price-text mb-0"><?php echo $price; ?></p>
+                                                        <div class="services-price-overlay"></div>
+                                                    </div>
+                                                </div>
+
+                                                <p><?php the_content(); ?></p>
+
+                                                <a href="#" class="custom-btn custom-border-btn btn mt-3">Discover More</a>
+
+                                                <div class="services-icon-wrap d-flex justify-content-center align-items-center">
+                                                    <?php
+                                                        $postid = $the_query->post->ID;
+
+                                                        $icon_class = get_post_meta($postid, 'icon_class_name', true);
+                                                    ?>
+                                                    <i class="<?php echo $icon_class; ?>"></i>
+                                                </div>
                                             </div>
                                         </div>
 
-                                        <p>You may want to explore Too CSS for great collection of free HTML CSS templates.</p>
+                                            
+                                        <?php endwhile; ?>
+                                        <!-- end of the loop -->
 
-                                        <a href="#" class="custom-btn custom-border-btn btn mt-3">Discover More</a>
+                                        <!-- pagination here -->
 
-                                        <div class="services-icon-wrap d-flex justify-content-center align-items-center">
-                                            <i class="services-icon bi-globe"></i>
-                                        </div>
-                                    </div>
-                                </div>
+                                        <?php wp_reset_postdata(); ?>
 
-                                <div class="col-lg-6 col-12">
+                                    <?php else : ?>
+                                        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                                    <?php endif; 
+                                // End the query (for Post Loop) 
+                                
+                                ?>
+
+                                <!-- <div class="col-lg-6 col-12">
                                     <div class="services-thumb services-thumb-up">
                                         <div class="d-flex flex-wrap align-items-center border-bottom mb-4 pb-3">
                                             <h3 class="mb-0">Branding</h3>
@@ -92,7 +136,7 @@
                                             <i class="services-icon bi-google"></i>
                                         </div>
                                     </div>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                     </div>
